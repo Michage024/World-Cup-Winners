e7b1a0cb3b5445b655b69cc0d208971c1b57ac5a
Michael Agesse <twzfi8mq@students.codeinstitute.net>
2024-07-15 16:20:50 +0000

    Add questions and answers

---
+++ base-project/script.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question: "What country won the FIFA world cup in 1970",
+        a: "Morocco",
+        b: "Portugal",
+        c: "U.S.A",
+        d: "Brazil",
+        correct: "d",
+    },
+    {
+        question: "What country won the FIFA world cup in 1938",
+        a: "England",
+        b: "Italy",
+        c: "Uruguay",
+        d: "Sweden",
+        correct: "b",
+    },
+    {
+        question: "What country won the FIFA world cup in 2018",
+        a: "France",
+        b: "Netherlands",
+        c: "Ireland",
+        d: "Germany",
+        correct: "a",
+    },
+    {
+        question: "What country won the FIFA world cup in 2010",
+        a: "Belgium",
+        b: "Spain",
+        c: "Croatia",
+        d: "Argentina",
+        correct: "b",
+    },
+];
